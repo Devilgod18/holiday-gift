e5c57e9940e568b18dfc3663f1995abd6b11e215
Connor Phipps <appeljed000@gmail.com>
2022-12-14 09:18:12 -0800

    more structure to api file

--- src/apis/GiftApi.js
+++ src/apis/GiftApi.js
@@ -4,7 +4,25 @@ const BASE = "http://localhost:8080";
 const URI = BASE + "/api"
 
 const GiftApi = {
-    //GET, POST, DELETE, PULL here
+    //getAll Request be made here
+    getAll: (setGiftList) => {
+
+    },
+
+    //add Request be made here
+    add: (gift) => {
+
+    },
+
+    //update Request be made here
+    update: (gift, giftList, setGiftList) => {
+
+    },
+
+    //delete Request be made here
+    delete: (id) => {
+
+    }
 }
 
 export default GiftApi;
