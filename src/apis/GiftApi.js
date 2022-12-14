f0666263c4a14e4e5191325904e1a524f52b1fa1
Connor Phipps <appeljed000@gmail.com>
2022-12-14 08:22:36 -0800

    added skeleton file for gift api

---
+++ src/apis/GiftApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BASE = "http://localhost:8080";
+const URI = BASE + "/api"
+
+const GiftApi = {
+    //GET, POST, DELETE, PULL here
+}
+
+export default GiftApi;
